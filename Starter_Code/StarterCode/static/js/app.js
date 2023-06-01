479b6ed25a1d827df8d0424dd1e3814fc0c4ae45
MatthewLeon1 <leonmw10@alumni.uww.edu>
2023-06-01 15:03:39 -0700

    updated bar chart

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -7,21 +7,27 @@ d3.json(url).then(function(data) {
 
     let person = data["samples"][0];
 
-    console.log(person);
+    console.log("person",person);
 
     // TODO: access proper data and format for plotly
     let trace1 = {
-        x: person["otu_ids"],
-        y: person["sample_values"],
-        type: 'bar'
+        x: person["sample_values"].slice(0,10).reverse(),
+        y: person["otu_ids"].slice(0,10).map(number => "OTU "+ number.toString()).reverse(),
+        text: person["otu_labels"].slice(0,10).reverse(),
+        type: 'bar',
+        orientation: "h"
     };
       
     let formattedData = [trace1];
       
-    title = "bellyButtonCRAP"
     let layout = {
-        title: title
-    };
+        margin: {
+          l: 100,
+          r: 100,
+          t: 100,
+          b: 100
+        }
+      };
 
     Plotly.newPlot("plot", formattedData, layout);
 });
